8bf41ce0d9d02da25f8f98268af538372a373922
Ricardo354 <yellow_richard@proton.me>
2025-09-01 19:01:05 -0300

    Solução

---
+++ array.py
@@ -0,0 +1,12 @@
+nomes = ['Igor', 'Francisco', 'Thamirys', 'Moura', 'Roni']
+
+print(nomes[0])
+print(nomes[2])
+print(nomes[-1])
+
+if "Carla" in nomes:
+    print("Carla está na lista!")
+else:
+    print("Carla não foi encontrada na lista!")
+
+    
